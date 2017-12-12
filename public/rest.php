fbdc7087337847d0cf3c3ffa49bc9fcf43a5bd8f
Steevan BARBOYON <steevan.barboyon@gmail.com>
2017-12-12 17:19:31 +0100

    Add phpbenchmarks/stats

--- public/rest.php
+++ public/rest.php
@@ -7,3 +7,5 @@ chdir(dirname(__DIR__));
 include __DIR__ . '/../vendor/autoload.php';
 $appConfig = require __DIR__ . '/../config/application.rest.config.php';
 Application::init($appConfig)->run();
+
+// require phpbenchmarks stats.php here when needed
